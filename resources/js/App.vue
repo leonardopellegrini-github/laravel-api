a5b6ce2eb89da5eb9d82b0cdd6b45947c8de9a65
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-07-14 18:05:10 +0200

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+
+  <h1>Questo è App</h1>
+
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data() {
+        return {
+            apiUrl: "http://127.0.0.1:8000/api/posts",
+            posts: []
+        }
+    },
+    methods: {
+        getApi() {
+            axios.get(this.apiUrl).then((r) => {
+                this.posts = r.data;
+                console.log(r.data)
+            });
+        },
+    },
+    mounted() {
+        this.getApi();
+    }
+
+}
+</script>
+
+<style>
+
+</style>
